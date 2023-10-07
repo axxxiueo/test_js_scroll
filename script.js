5dc0f41addddcff61e24d2d6f6bbbd5afcf13d98
axxxiueo <77669299+axxxiueo@users.noreply.github.com>
2023-10-07 22:11:12 +0900

    completed

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", function(){
+    let scroll = document.documentElement.scrollTop;
+    document.getElementById("scrollValue").textContent = scroll;
+    if (scroll > 300){
+        document.querySelector("main").classList.add("test");
+    } else {
+        document.querySelector("main").classList.remove("test");
+    };
+});
